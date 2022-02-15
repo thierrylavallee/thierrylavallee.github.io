4e682822e5a8fde566df71ae38a8452d24fac0b0
Thierry Lavallée <6714261+thierrylavallee@users.noreply.github.com>
2022-02-15 20:25:03 +0100

    changes

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/** @format */
+
+// $('#element01').animate(
+//     {
+//         opacity: 1,
+//         height: '100%',
+//         top: '0%'
+//     },
+//     5000,
+//     function () {
+//         $('#element01').animate(
+//             {
+//                 opacity: 0,
+//                 height: '0%',
+//                 top: '+=100%',
+//             },
+//             5000
+//         );
+//     }
+// );
+
+var d = new Date();
+$(".thirdCopyYear").text(d.getFullYear());
